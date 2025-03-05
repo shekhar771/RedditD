0d4a98e3b85ee4317529d7c9d9817c229eeb8ef2
shekhar771 <ashekhar.1239@gmail.com>
2025-03-05 21:41:03 +0530

    worked

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -5,8 +5,13 @@ import { useState } from "react";
 const Page = () => {
   const [input, setInput] = useState<string>("");
   return (
-    <div>
-      <h1>Create Page</h1>
+    <div className="  container flex items-center h-full max-w-3xl mx-auto ">
+      <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-semfold">Create a subreddit</h1>
+        </div>
+      </div>
+
       <p>This is a starter template for a Next.js page using TypeScript.</p>
     </div>
   );
